0a8c3e124a1488636e27695fdb12a284a1038e55
TheBlackSurf <anonsurfqwertyuiop@gmail.com>
2022-07-02 09:56:57 +0200

    update settings

--- betting/settings.py
+++ betting/settings.py
@@ -137,18 +137,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-# STATIC_URL = 'static/'
-# MEDIA_URL = '/media/'
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     '/home/srv37714/static/',
-# ]
-# STATIC_ROOT='/home/srv37714/public_html/static'
-# MEDIA_ROOT='/home/srv37714/public_html/media'
 
 
 STATIC_URL = '/static/'
